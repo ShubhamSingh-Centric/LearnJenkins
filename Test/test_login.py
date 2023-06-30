892a9e482cdea6d816533667999bff2afdb284e8
shubham.singh <shubham.singh@centricconsulting.com>
2023-06-30 14:59:58 +0530

    commit#2

---
+++ Test/test_login.py
@@ -0,0 +1,17 @@
+import time
+from main import LoginPage, HomePage
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+
+
+def test_login_user():
+    page = LoginPage(driver)
+    page.go()
+    page.input_user_name("student")
+    page.input_password("Password123")
+    page.click_submit_btn()
+    time.sleep(5)
+    homepage = HomePage(driver)
+    assert homepage.heading_text == "Logged In Successfully"
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import time
-
-from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,39 +5,50 @@ from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://practicetestautomation.com/practice-test-login/'
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get(URL)
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def go(self):
+        self.driver.maximize_window()
+        self.driver.get(URL)
+
+    def input_user_name(self, username):
+        x_path = '//input[@id="username"]'
+        locator = (By.XPATH, x_path)
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        elm.send_keys(username)
+        return None
+
+    def input_password(self, password):
+        x_path = '//input[@id="password"]'
+        locator = (By.XPATH, x_path)
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        elm.send_keys(password)
+        return None
+
+    def click_submit_btn(self):
+        x_path = '//button[@id="submit"]'
+        locator = (By.XPATH, x_path)
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        elm.click()
+        return None
 
 
-# comment 2
-def input_user_name(username):
-    x_path = '//input[@id="username"]'
-    locator = (By.XPATH, x_path)
-    elm = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(locator))
-    elm.send_keys(username)
-    return None
+class HomePage:
 
+    def __init__(self, driver):
+        self.driver = driver
 
-def input_password(password):
-    x_path = '//input[@id="password"]'
-    locator = (By.XPATH, x_path)
-    elm = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(locator))
-    elm.send_keys(password)
-    return None
-
-
-def click_submit_btn():
-    x_path = '//button[@id="submit"]'
-    locator = (By.XPATH, x_path)
-    elm = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(locator))
-    # elm = driver.find_element(locator)
-    elm.click()
-    return None
+    @property
+    def heading_text(self):
+        x_path = '//h1[@class="post-title"]'
+        locator = (By.XPATH, x_path)
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        return elm.text
 
 
 if __name__ == "__main__":
-    input_user_name("student")
-    input_password("Password123")
-    click_submit_btn()
-    time.sleep(5)
+    pass
